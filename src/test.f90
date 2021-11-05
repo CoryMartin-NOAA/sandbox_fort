40ed2dcafc76403e37848f9def15b0b645c410bf
Cory Martin <cory.r.martin@noaa.gov>
2021-11-04 21:36:52 -0500

    fix fortran

--- src/test.f90
+++ src/test.f90
@@ -2,12 +2,12 @@ program githubci
   implicit none
   real :: a, b, y
   integer :: i
-  
+
   a = 0.5
   b = 1
   do i=1,10
     y = a*float(i) + b
     print *, y
-  done  
-  
+  end do
+
 end program githubci
